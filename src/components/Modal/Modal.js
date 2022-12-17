ac412c2ad292dc2d7869cae22e835d85ff89c423
thanh long <dtlong943@gmail.com>
2022-12-18 03:11:39 +0700

    up load code

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -22,52 +22,52 @@ function Modal() {
            <div className={cx('wrapper')}>
             <div className={cx('close_wrap')}>
                 <button onClick={handleCloseModal}>
-                    <CloseIconModal width='25px' height='25px' className={cx('icon')}/>
+                    <CloseIconModal width='25px' height='25px' className={cx('close_icon')}/>
                 </button>
             </div>
            <div className={cx('body')}>
-                <h1 >Đăng nhập vào Tiktok</h1>
                 <ul className={cx('login_list')}>
+                <h1 >Đăng nhập vào Tiktok</h1>
                     <li className={cx('login_item')}>
-                        <QRicon width='2rem' height='2rem'/>
+                        <QRicon width='2rem' height='2rem' className={cx('icon')}/>
                         Sử dụng mã QR
                     </li>
                     <li className={cx('login_item')}>
-                        <UserIcon width='2rem' height='2rem'/>
+                        <UserIcon width='2rem' height='2rem' className={cx('icon')}/>
                         Số điện thoại / Email / Tiktok ID
                     </li>
                     <li className={cx('login_item')}>
-                        <FacebookIcon width='2rem' height='2rem'/>
+                        <FacebookIcon width='2rem' height='2rem' className={cx('icon')}/>
                         Tiếp tục với Facebook
                     </li>
                     <li className={cx('login_item')}>
-                        <GoogleIcon width='2rem' height='2rem'/>
+                        <GoogleIcon width='2rem' height='2rem' className={cx('icon')}/>
                         Tiếp tục với Google
                     </li>
                     <li className={cx('login_item')}>
-                        <TwitterIcon width='2rem' height='2rem'/>
+                        <TwitterIcon width='2rem' height='2rem' className={cx('icon')}/>
                         Tiếp tục với Twitter
                     </li>
                     <li className={cx('login_item')}>
-                        <LineIcon width='2rem' height='2rem'/>
+                        <LineIcon width='2rem' height='2rem' className={cx('icon')}/>
                         Tiếp tục với Line
                     </li>
                     <li className={cx('login_item')}>
-                        <KakaotalkIcon width='2rem' height='2rem'/>
+                        <KakaotalkIcon width='2rem' height='2rem' className={cx('icon')}/>
                         Tiếp tục với Kakaotalk
                     </li>
                     <li className={cx('login_item')}>
-                        <AppleIcon width='2rem' height='2rem'/>
+                        <AppleIcon width='2rem' height='2rem' className={cx('icon')}/>
                         Tiếp tục với Apple
                     </li>
                     <li className={cx('login_item')}>
-                        <InstagramIcon width='2rem' height='2rem'/>
+                        <InstagramIcon width='2rem' height='2rem' className={cx('icon')}/>
                         Tiếp tục với Instagram
                     </li>
                 </ul>
             </div> 
             <div className={cx('footer')}>
-                <div className={cx('use_terms')}>Điều khoản sử dụng</div>
+                {/* <div className={cx('use_terms')}>Điều khoản sử dụng</div> */}
                 <div className={cx('footer_title')}>Bạn không có tài khoản? <span onClick={toggleChangeModal}>Đăng ký</span></div> 
             </div>
             </div>
